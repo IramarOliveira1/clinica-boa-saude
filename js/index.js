2e51b5328a0841c2f8c76019dd61e262b58bd5bd
IRAMAR CAPIM DE OLIVEIRA <iramar.capim@viavarejo.com.br>
2022-11-04 21:40:11 -0300

    change api key

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ $('#form-send-mail').submit((e) => {
     const headers = {
         Accept: "application/json",
         "Content-Type": 'application/json',
-        "api-key": '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',
+        "api-key": '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',
         "X-Mailin-custom": "custom_header_1:custom_value_1|custom_header_2:custom_value_2|custom_header_3:custom_value_3",
         charset: "iso-8859-1"
     }
@@ -51,7 +51,7 @@ $('#form-send-mail').submit((e) => {
     }
 
     const sender = {
-        email: $('input[name=email]').val()
+        email: '"mazinholiveira5@gmail.com'
     };
 
     const data = {
